ddeed20a5812bf9d733af85f241ca89b3d35ddff
nickunger99 <nicolas.unger@yahoo.com.br>
2022-11-01 20:25:54 -0300

    Mentoria e Curso

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Curso curso1 = new Curso();
+        curso1.setTitulo("curso java");
+        curso1.setDescricao("descricao curso java");
+        curso1.setCargaHoraria(4);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("curso java");
+        curso2.setDescricao("descricao curso java");
+        curso2.setCargaHoraria(4);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("mentoria java");
+        mentoria.setDescricao("descrição mentoria java");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria);
     }
 }
